960cffef9d5cc53717b1aa555958f65265668e74
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2019-02-05 17:58:03 +0100

    Add CachedNormalizedModifiedPurity (I hope not to regret about it)

---
+++ src/main/java/org/nlpub/watset/eval/CachedNormalizedModifiedPurity.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Dmitry Ustalov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.nlpub.watset.eval;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * {@inheritDoc}
+ * <p>
+ * This class creates an instance of {@link ConcurrentHashMap} to cache the scores
+ * for the elements of clusters. It is designed under an assumption that the
+ * <tt>classes</tt> <i>do not change</i>. This assumption is valid for computing
+ * the precision (nmPU) in case of normalized modified purity as defined by
+ * <a href="https://doi.org/10.3115/v1/P14-1097">Kawahara et al. (ACL 2014)</a>.
+ * <p>
+ * Since the underlying data structure has no size limit, so this class is memory-greedy.
+ * Please make sure that you have tuned the JVM heap size when using it.
+ */
+public class CachedNormalizedModifiedPurity<V> extends NormalizedModifiedPurity<V> {
+    protected final Map<Map<V, Double>, Double> cache;
+
+    /**
+     * {@inheritDoc}
+     */
+    public CachedNormalizedModifiedPurity() {
+        this(true, true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public CachedNormalizedModifiedPurity(boolean normalized, boolean modified) {
+        super(normalized, modified);
+        this.cache = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public double score(Map<V, Double> cluster, Collection<Map<V, Double>> classes) {
+        return cache.computeIfAbsent(cluster, c -> super.score(cluster, classes));
+    }
+}
---
+++ src/test/java/org/nlpub/watset/eval/CachedNormalizedModifiedPurityTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2019 Dmitry Ustalov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.nlpub.watset.eval;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.nlpub.watset.eval.NormalizedModifiedPurityTest.*;
+
+public class CachedNormalizedModifiedPurityTest {
+    private static final NormalizedModifiedPurity<String> mpu = new CachedNormalizedModifiedPurity<>(false, true);
+    private static final NormalizedModifiedPurity<String> pu = new NormalizedModifiedPurity<>(false, false);
+
+    private static final NormalizedModifiedPurity<String> nmpu = new CachedNormalizedModifiedPurity<>();
+    private static final NormalizedModifiedPurity<String> npu = new NormalizedModifiedPurity<>(true, false);
+
+    @Test
+    public void testGold() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(mpu, pu, GOLD, GOLD);
+        assertEquals(1, result.getPrecision(), .0001);
+        assertEquals(1, result.getRecall(), .0001);
+        assertEquals(1, result.getF1Score(), .0001);
+    }
+
+    @Test
+    public void testGoldNormalized() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(nmpu, npu, GOLD_NORMALIZED, GOLD_NORMALIZED);
+        assertEquals(1, result.getPrecision(), .0001);
+        assertEquals(1, result.getRecall(), .0001);
+        assertEquals(1, result.getF1Score(), .0001);
+    }
+
+    @Test
+    public void testExample1() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(mpu, pu, EXAMPLE_1, GOLD);
+        assertEquals(.71429, result.getPrecision(), .0001);
+        assertEquals(.71429, result.getRecall(), .0001);
+        assertEquals(.71429, result.getF1Score(), .0001);
+    }
+
+    @Test
+    public void testExample1Normalized() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(nmpu, npu, EXAMPLE_1_NORMALIZED, GOLD_NORMALIZED);
+        assertEquals(.75, result.getPrecision(), .0001);
+        assertEquals(.75, result.getRecall(), .0001);
+        assertEquals(.75, result.getF1Score(), .0001);
+    }
+
+    @Test
+    public void testExample2() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(mpu, pu, EXAMPLE_2, GOLD);
+        assertEquals(.66667, result.getPrecision(), .0001);
+        assertEquals(1, result.getRecall(), .0001);
+        assertEquals(.80000, result.getF1Score(), .0001);
+    }
+
+    @Test
+    public void testExample2Normalized() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(nmpu, npu, EXAMPLE_2_NORMALIZED, GOLD_NORMALIZED);
+        assertEquals(.66667, result.getPrecision(), .0001);
+        assertEquals(1, result.getRecall(), .0001);
+        assertEquals(.80000, result.getF1Score(), .0001);
+    }
+
+    @Test
+    public void testExample3() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(mpu, pu, EXAMPLE_3, GOLD);
+        assertEquals(0, result.getPrecision(), .0001);
+        assertEquals(.28571, result.getRecall(), .0001);
+        assertEquals(0, result.getF1Score(), .0001);
+    }
+
+    @Test
+    public void testExample3Normalized() {
+        final PrecisionRecall result = NormalizedModifiedPurity.evaluate(nmpu, npu, EXAMPLE_3_NORMALIZED, GOLD_NORMALIZED);
+        assertEquals(0, result.getPrecision(), .0001);
+        assertEquals(.33333, result.getRecall(), .0001);
+        assertEquals(0, result.getF1Score(), .0001);
+    }
+}
