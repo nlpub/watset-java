5d5add279a8edda0bdcf765a4eda5b1b3415dfb6
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-01-09 22:02:23 +0500

    Add CommandLine.sh and java.tsv
    The java.tsv dataset is the same graph as in Figure 16 in doi:10.1162/coli_a_00354,
    but the edges are weighted using the cosine similarity between the corresponding
    Google News word embeddings. Edges of OOV entries use the average weight.

---
+++ examples/CommandLine.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+WATSET=${WATSET-watset.jar}
+INPUT=${INPUT-java.tsv}
+
+set -ex
+
+# Chinese Whispers
+for mode in top lin log ; do
+  java -jar $WATSET -i $INPUT cw -m $mode
+done
+
+# Markov Clustering
+java -jar $WATSET -i $INPUT mcl
+
+# MaxMax
+java -jar $WATSET -i $INPUT maxmax
+
+# Watset Sense Induction
+java -jar $WATSET -i $INPUT senses -s -l mcl
+
+for lmode in top lin log ; do
+  java -jar $WATSET -i $INPUT senses -s -l cw -lp mode=$lmode
+done
+
+# Watset Clustering
+java -jar $WATSET -i $INPUT watset -s -l mcl -g mcl # Watset[MCL, MCL]
+
+for gmode in top lin log ; do
+  java -jar $WATSET -i $INPUT watset -s -l mcl -g cw -gp mode=$gmode # Watset[MCL, CW]
+done
+
+for lmode in top lin log ; do
+  java -jar $WATSET -i $INPUT watset -s -l cw -lp mode=$lmode -g mcl # Watset[CW, MCL]
+
+  for gmode in top lin log ; do
+    java -jar $WATSET -i $INPUT watset -s -l cw -lp mode=$lmode -g cw -gp mode=$gmode # Watset[CW, CW]
+  done
+done
