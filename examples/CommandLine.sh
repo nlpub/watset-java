08ee6585ce5f582d1caa5b889855a06247b61620
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-02-07 23:04:09 +0300

    Double quotes in CommandLine.sh

--- examples/CommandLine.sh
+++ examples/CommandLine.sh
@@ -6,47 +6,47 @@ INPUT=${INPUT-java.tsv}
 set -ex
 
 # Dummy Clustering
-java -jar $WATSET -i $INPUT empty
-java -jar $WATSET -i $INPUT singleton
-java -jar $WATSET -i $INPUT together
-java -jar $WATSET -i $INPUT components
+java -jar "$WATSET" -i "$INPUT" empty
+java -jar "$WATSET" -i "$INPUT" singleton
+java -jar "$WATSET" -i "$INPUT" together
+java -jar "$WATSET" -i "$INPUT" components
 
 # Chinese Whispers
 for mode in top lin log ; do
-  java -jar $WATSET -i $INPUT cw -m $mode
+  java -jar "$WATSET" -i "$INPUT" cw -m $mode
 done
 
 # Markov Clustering
-java -jar $WATSET -i $INPUT mcl
+java -jar "$WATSET" -i "$INPUT" mcl
 
 # MaxMax
-java -jar $WATSET -i $INPUT maxmax
+java -jar "$WATSET" -i "$INPUT" maxmax
 
 # Watset Sense Induction
-java -jar $WATSET -i $INPUT senses -s -l mcl
+java -jar "$WATSET" -i "$INPUT" senses -s -l mcl
 
 for lmode in top lin log ; do
-  java -jar $WATSET -i $INPUT senses -s -l cw -lp mode=$lmode
+  java -jar "$WATSET" -i "$INPUT" senses -s -l cw -lp mode=$lmode
 done
 
 # Watset Sense Graph
-java -jar $WATSET -i $INPUT graph -s -l mcl
+java -jar "$WATSET" -i "$INPUT" graph -s -l mcl
 
 for lmode in top lin log ; do
-  java -jar $WATSET -i $INPUT graph -s -l cw -lp mode=$lmode
+  java -jar "$WATSET" -i "$INPUT" graph -s -l cw -lp mode=$lmode
 done
 
 # Watset Clustering
-java -jar $WATSET -i $INPUT watset -s -l mcl -g mcl # Watset[MCL, MCL]
+java -jar "$WATSET" -i "$INPUT" watset -s -l mcl -g mcl # Watset[MCL, MCL]
 
 for gmode in top lin log ; do
-  java -jar $WATSET -i $INPUT watset -s -l mcl -g cw -gp mode=$gmode # Watset[MCL, CW]
+  java -jar "$WATSET" -i "$INPUT" watset -s -l mcl -g cw -gp mode=$gmode # Watset[MCL, CW]
 done
 
 for lmode in top lin log ; do
-  java -jar $WATSET -i $INPUT watset -s -l cw -lp mode=$lmode -g mcl # Watset[CW, MCL]
+  java -jar "$WATSET" -i "$INPUT" watset -s -l cw -lp mode=$lmode -g mcl # Watset[CW, MCL]
 
   for gmode in top lin log ; do
-    java -jar $WATSET -i $INPUT watset -s -l cw -lp mode=$lmode -g cw -gp mode=$gmode # Watset[CW, CW]
+    java -jar "$WATSET" -i "$INPUT" watset -s -l cw -lp mode=$lmode -g cw -gp mode=$gmode # Watset[CW, CW]
   done
 done
