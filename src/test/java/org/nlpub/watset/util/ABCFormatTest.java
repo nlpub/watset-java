b5ed92b581cc2894745b0e2a9fe27ca19d3d5420
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-07-19 18:35:49 +0300

    Add ABCFormatTest

---
+++ src/test/java/org/nlpub/watset/util/ABCFormatTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Dmitry Ustalov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.nlpub.watset.util;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ABCFormatTest extends TestCase {
+    final static URL EDGE_TSV = ABCFormatTest.class.getResource("edge.tsv");
+
+    public void testParse() throws IOException, URISyntaxException {
+        final var graph = ABCFormat.parse(Files.lines(Path.of(EDGE_TSV.toURI())));
+
+        assertEquals(2, graph.vertexSet().size());
+        assertEquals(1, graph.edgeSet().size());
+    }
+
+    public void testParseRegex() throws URISyntaxException, IOException {
+        final var graph = ABCFormat.parse(Files.lines(Path.of(EDGE_TSV.toURI())), " ");
+
+        assertEquals(0, graph.vertexSet().size());
+        assertEquals(0, graph.edgeSet().size());
+    }
+}
