8e95e50ee0a0a1d2d8cbd0806fa91d0c92e7f97b
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-08-09 20:10:35 +0300

    Simplify KSpanningTreeCommand

--- src/main/java/org/nlpub/watset/cli/KSpanningTreeCommand.java
+++ src/main/java/org/nlpub/watset/cli/KSpanningTreeCommand.java
@@ -1,11 +1,8 @@
 package org.nlpub.watset.cli;
 
 import com.beust.jcommander.Parameter;
+import org.jgrapht.alg.clustering.KSpanningTreeClustering;
 import org.jgrapht.alg.interfaces.ClusteringAlgorithm;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.nlpub.watset.util.ClusteringAlgorithmProvider;
-
-import java.util.Map;
 
 /**
  * A command that runs the <em>k</em> spanning tree clustering algorithm.
@@ -26,7 +23,6 @@ class KSpanningTreeCommand extends ClusteringCommand {
 
     @Override
     public ClusteringAlgorithm<String> getAlgorithm() {
-        final var params = Map.of("k", String.valueOf(k));
-        return new ClusteringAlgorithmProvider<String, DefaultWeightedEdge>("kst", params).apply(getGraph());
+        return new KSpanningTreeClustering<>(getGraph(), k);
     }
 }
