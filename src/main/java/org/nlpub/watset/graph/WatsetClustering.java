288dc0403c7dda45b4e60a5578928fb3e69c6c52
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-08-21 21:56:36 +0300

    Move WatsetClusteringImpl to WatsetClustering

--- src/main/java/org/nlpub/watset/graph/WatsetClustering.java
+++ src/main/java/org/nlpub/watset/graph/WatsetClustering.java
@@ -22,7 +22,9 @@ import org.jgrapht.alg.interfaces.ClusteringAlgorithm;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.nlpub.watset.util.Sense;
 
-import java.util.Map;
+import java.util.*;
+
+import static java.util.Objects.isNull;
 
 /**
  * A Watset clustering.
@@ -44,4 +46,81 @@ public interface WatsetClustering<V> extends ClusteringAlgorithm.Clustering<V> {
      * @return the disambiguated contexts
      */
     Map<Sense<V>, Map<Sense<V>, Number>> getContexts();
+
+    /**
+     * A Watset clustering that computes disambiguated contexts on demand.
+     *
+     * @param <V> the type of nodes in the graph
+     */
+    class WatsetClusteringImpl<V> extends ClusteringAlgorithm.ClusteringImpl<V> implements WatsetClustering<V> {
+        /**
+         * The sense inventory.
+         */
+        private final Map<V, Map<V, Integer>> inventory;
+
+        /**
+         * The sense graph.
+         */
+        private final Graph<Sense<V>, DefaultWeightedEdge> senseGraph;
+
+        /**
+         * The cached disambiguated contexts.
+         */
+        private Map<Sense<V>, Map<Sense<V>, Number>> contexts;
+
+        /**
+         * Construct a new Watset clustering.
+         *
+         * @param clusters   the clusters
+         * @param inventory  the sense inventory
+         * @param senseGraph the sense graph
+         */
+        public WatsetClusteringImpl(List<Set<V>> clusters, Map<V, Map<V, Integer>> inventory, Graph<Sense<V>, DefaultWeightedEdge> senseGraph) {
+            super(clusters);
+            this.inventory = inventory;
+            this.senseGraph = senseGraph;
+        }
+
+        /**
+         * Get the sense inventory built during {@link Watset#getClustering()}.
+         *
+         * @return the sense inventory
+         */
+        @SuppressWarnings("unused")
+        public Map<V, Map<V, Integer>> getInventory() {
+            return inventory;
+        }
+
+        @Override
+        public Graph<Sense<V>, DefaultWeightedEdge> getSenseGraph() {
+            return senseGraph;
+        }
+
+        @Override
+        public Map<Sense<V>, Map<Sense<V>, Number>> getContexts() {
+            if (isNull(contexts)) {
+                contexts = new HashMap<>(senseGraph.vertexSet().size());
+
+                for (final var edge : senseGraph.edgeSet()) {
+                    final Sense<V> source = senseGraph.getEdgeSource(edge), target = senseGraph.getEdgeTarget(edge);
+                    final var weight = senseGraph.getEdgeWeight(edge);
+
+                    if (!contexts.containsKey(source)) contexts.put(source, new HashMap<>());
+                    if (!contexts.containsKey(target)) contexts.put(target, new HashMap<>());
+
+                    contexts.get(source).put(target, weight);
+                    contexts.get(target).put(source, weight);
+                }
+
+                if (contexts.size() != senseGraph.vertexSet().size()) {
+                    throw new IllegalStateException("Mismatch in number of senses: expected " +
+                            senseGraph.vertexSet().size() +
+                            ", but got " +
+                            contexts.size());
+                }
+            }
+
+            return Collections.unmodifiableMap(contexts);
+        }
+    }
 }
